839fd6b0f2dc6ba834779e9ecb9e6fda750ff448
x.mat1 <mat12345duq6789@gmail.com>
2025-04-20 14:30:34 -0400

    v0.4

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ AllowedVehiclesList = [
     'Chevrolet Silverado',
     'Tesla CyberTruck',
     'Toyota Tundra',
-    'Nissan Titan'
+    'Nissan Titan',
+    'Rivian R1T',
+    'Ram 1500'
 ]
 
 def show_banner():
     print("********************************")
-    print("AutoCountry Vehicle Finder v0.3")
+    print("AutoCountry Vehicle Finder v0.4")
     print("********************************")
 
 def show_menu():
@@ -18,7 +20,8 @@ def show_menu():
     print("1. PRINT all Authorized Vehicles")
     print("2. SEARCH for Authorized Vehicle")
     print("3. ADD Authorized Vehicle")
-    print("4. Exit")
+    print("4. DELETE Authorized Vehicle")
+    print("5. Exit")
 
 def print_allowed_vehicles():
     print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
@@ -39,6 +42,20 @@ def add_vehicle():
     AllowedVehiclesList.append(new_vehicle)
     print(f'\nYou have added "{new_vehicle}" as an authorized vehicle')
 
+def delete_vehicle():
+    print("********************************")
+    vehicle_to_remove = input("Please Enter the full Vehicle name you would like to REMOVE:\n").strip()
+    
+    if vehicle_to_remove in AllowedVehiclesList:
+        confirmation = input(f'Are you sure you want to remove "{vehicle_to_remove}" from the Authorized Vehicles List?\n').strip().lower()
+        if confirmation == 'yes':
+            AllowedVehiclesList.remove(vehicle_to_remove)
+            print(f'\nYou have REMOVED "{vehicle_to_remove}" as an authorized vehicle')
+        else:
+            print("")
+    else:
+        print(f'\n"{vehicle_to_remove}" is not currently in the Authorized Vehicles List.')
+
 def main():
     while True:
         show_banner()
@@ -52,11 +69,13 @@ def main():
         elif choice == '3':
             add_vehicle()
         elif choice == '4':
+            delete_vehicle()
+        elif choice == '5':
             print("\nThank you for using the AutoCountry Vehicle Finder, good-bye!")
             input("\nPress Enter to close the program...")
             break
         else:
-            print("\nInvalid option. Please enter 1, 2, 3, or 4.\n")
+            print("\nInvalid option. Please enter a number from 1 to 5.\n")
 
 if __name__ == "__main__":
     main()
